9934970c6a3329375af4a063df6cd26feb93b2a0
Arux26 <arieltrangoni1@gmail.com>
2022-11-30 01:45:23 -0300

    restore folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        HOLAAAA
       </header>
     </div>
   );
