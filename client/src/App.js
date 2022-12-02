ad01c2050f7b46c4c321fb2c97db775b29a257e7
gtorresgrau <gonzalotorresgrau@gmail.com>
2022-12-02 14:51:39 -0300

    Components, App, Index

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-//import logo from './logo.svg';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Components/Landing.jsx';
+import Home from './Components/Home.jsx';
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        HOLAAAA
-      </header>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/" component={Landing}/>
+        <Route path="*" component={Error404}/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import {store} from './Store/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Provider>,
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
