44ac4d43ef22cccbf83e3110e9f30c1647110c7f
Arux26 <arieltrangoni1@gmail.com>
2022-12-04 14:37:23 -0300

    Sneaker Detail

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Components/Landing.jsx';
 import Home from './Components/Home.jsx';
 import Error404 from './Components/Error404';
+import SneakerDetail from './Components/SneakerDetail.jsx';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/home" component={Home} />
           <Route exact path="/" component={Landing} />
+          <Route exact path="/sneaker/:id" component={SneakerDetail} />
           <Route path="*" component={Error404} />
         </Switch>
       </div>
