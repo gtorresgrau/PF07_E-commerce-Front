4d8887fab2527ace5fbf2efc2623266089503ba8
Arux26 <arieltrangoni1@gmail.com>
2022-12-06 00:54:29 -0300

    Correciones, funcionando 100%

--- client/src/Components/SneakerDetail.jsx
+++ client/src/Components/SneakerDetail.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { getSneakerDetail, resetDetail } from '../Actions/Actions';
 import Loading from './Loading';
 import s from './Styles/Detail.module.css';
-
+import Navbar from './NavBar';
 
 
 export default function SneakerDetail() {
@@ -23,36 +23,27 @@ export default function SneakerDetail() {
 
   return (
     <div>
+      <Navbar />
       {loading ? <Loading /> :
         <div className={s.containerG}>
-          <div className={s.container1}>
-            <Link to="/sneakers"><button className={s.btn}>← BACK</button></Link>
-            <br />
-            <div className={s.principal}>
-              <h2 className={s.principal}>{sneaker.brand}</h2>
-              <h2 className={s.principal}>{sneaker.title}</h2>
-              <img className={s.img} src={sneaker.image} alt="img not found" witdh="200px" height="200px" />
-              <br />
-              <h3>$ <span>{sneaker.price}</span></h3>
-              <h3>Size: <span>{sneaker.size}</span></h3>
+          <div className={s.containerimg}>
+            <img className={s.img} src={sneaker.image} alt="img not found" />
+          </div>
+
+          <div className={s.containertext}>
+            <div className={s.title}>
+              <h1>{sneaker.title}</h1>
+              <h2>{sneaker.brand}</h2>
+              <h2>${sneaker.price}</h2>
+              {/* <h3>Size: <span>{sneaker.size}</span></h3>
               <h3>Stock: <span>{sneaker.stock}</span></h3>
               <h3>Colour: <span>{sneaker.colour}</span></h3>
-              <h3>Genre: <span>{sneaker.genre}</span></h3>
-              <span>{sneaker.description}</span>
+              <h3>Genre: <span>{sneaker.genre}</span></h3> */}
+              <p>{sneaker.description}</p>
+              <Link to="/sneakers"><button className={s.btn}>← BACK</button></Link>
             </div>
           </div>
-          {/*    <div className={s.container2}>
-              <h4 className={s.activities}><h3 className={s.textAct}>Tourist activities: </h3>{sneaker.activities.length ? sneaker.activities.map(e => {
-                return (
-                  <ul className={s.ull}>
-                    <li>Name: <span>{e.nombre}</span></li>
-                    <li>Difficulty: <span>{e.dificultad}/5</span></li>
-                    <li>Duration: <span>{e.duracion}hs.</span></li>
-                    <li>Season: <span>{e.temporada}</span></li>
-                  </ul>
-                )
-              }) : <span>Has no activities</span>}</h4>
-            </div> */}
+
         </div>
 
       }
