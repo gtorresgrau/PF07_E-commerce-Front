0b451467c5634118538b76611e14dcc8b7fb5325
gtorresgrau <gonzalotorresgrau@gmail.com>
2022-12-13 15:18:34 -0300

    deploy

--- client/src/Components/Logoutbutton.jsx
+++ client/src/Components/Logoutbutton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import S from './Styles/NavBar.module.css'
+//import S from './Styles/NavBar.module.css'
 
 export const LogoutButton = () => {
   const { logout } = useAuth0();
