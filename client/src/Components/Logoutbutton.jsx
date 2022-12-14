ada8944ac9e781606bf45d50a3195bc7876b69d7
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2022-12-14 00:19:32 -0300

    done

--- client/src/Components/Logoutbutton.jsx
+++ client/src/Components/Logoutbutton.jsx
@@ -6,13 +6,9 @@ export const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-<<<<<<< HEAD
+    
     <button className={S.singInButton} onClick={() => logout({ returnTo: "http://localhost:3000/sneakers"||"https://pf-07-e-commerce-front-e03qnrsav-gtorresgrau.vercel.app/" })}>
-=======
-
-    <div onClick={() => logout({ returnTo: "https://pf-07-e-commerce-front.vercel.app/sneakers/"})}>
->>>>>>> 0b451467c5634118538b76611e14dcc8b7fb5325
       Log out
-    </div>
+    </button>
   );
 };
--- client/src/Components/UserLogin.jsx
+++ client/src/Components/UserLogin.jsx
@@ -3,27 +3,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 import S from './Styles/NavBar.module.css'
 
 export const UserLogin = () => {
-<<<<<<< HEAD
-  const { user} = useAuth0();
-
-const  img = user.picture ;
-
-  
-=======
   const { user } = useAuth0();
 
   const img = user.picture ;
->>>>>>> 0b451467c5634118538b76611e14dcc8b7fb5325
   
   return (
 
       <div>
 
-<<<<<<< HEAD
-        <img className={S.img} src={img} alt="user.img" />
-=======
           <img className={S.userimage} src={user.picture} alt=""/>
->>>>>>> 0b451467c5634118538b76611e14dcc8b7fb5325
 
       </div>
 
