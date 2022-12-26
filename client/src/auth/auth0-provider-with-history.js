a71afc1d3dccc6eb059c95d906cb5b6768776668
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2022-12-26 05:47:45 -0300

    favoritos

--- client/src/auth/auth0-provider-with-history.js
+++ client/src/auth/auth0-provider-with-history.js
@@ -5,7 +5,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
   const history = useHistory();
 
