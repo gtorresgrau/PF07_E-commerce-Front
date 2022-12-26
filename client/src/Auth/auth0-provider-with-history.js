f82e75f93882ff84315548590b2317efa3c853d7
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2022-12-26 06:42:42 -0300

    a

--- client/src/Auth/auth0-provider-with-history.js
+++ client/src/Auth/auth0-provider-with-history.js
@@ -16,7 +16,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+    redirectUri={"http://localhost:3000/sneakers"}
       onRedirectCallback={onRedirectCallback}
     >
       {children}
