eb47aed5b8038a9b96f0cac1ab517df81ea14b3d
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2023-01-15 02:20:39 -0300

    last

--- client/src/Components/CheckoutForm.jsx
+++ client/src/Components/CheckoutForm.jsx
@@ -20,12 +20,10 @@ export function CheckoutForm() {
   const { cartItems } = useContext(CartContex);
   const [errors, setErrors] = useState({});
 
-  const history = useHistory()
+  
+  //const users = useSelector((state) => state.users)
 
-  const users = useSelector((state) => state.users)
-
-  const userBaned = users.find(e => e.email === user.user.email)
-  console.log("USERBANNED", userBaned)
+  
 
   const [formData, setFormData] = useState({
     fullName: user.user.name || '',
@@ -70,8 +68,7 @@ export function CheckoutForm() {
 
   return (
     <div className={S.general}>
-      {userBaned.isBanned ? alert("You have been banned!", history.push("/sneakers"))
-        :
+      
         <div className={S.container}>
           <div>
             <Link to="/sneakers"><button className={S.back} >← BACK TO CART</button></Link>
@@ -136,7 +133,7 @@ export function CheckoutForm() {
             <button type="submit" onClick={handleSubmit} className={S.btnBuy} disabled={!formData.city || formData.phoneNumber.length < 7 || formData.phoneNumber.length > 8 || !formData.region || !formData.homeAddress}>BUY</button>
           </form>
         </div >
-      }
+      
     </div >
   )
 
--- client/src/Components/OrderHistory.jsx
+++ client/src/Components/OrderHistory.jsx
@@ -1,86 +1,83 @@
-import * as React from 'react';import { useEffect } from "react"
-import { useDispatch, useSelector } from 'react-redux';
+import * as React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllOrders } from "../Actions/Actions";
-import { useAuth0 } from '@auth0/auth0-react';
-import TableCell from '@mui/material/TableCell';
+import { useAuth0 } from "@auth0/auth0-react";
+import TableCell from "@mui/material/TableCell";
 
 const OrderHistory = () => {
+  const Orders = useSelector((state) => state.orders);
+  const dispatch = useDispatch();
+  const { user } = useAuth0();
 
-    const Orders = useSelector((state) => state.orders);
-    const dispatch = useDispatch();
-    const { user } = useAuth0();
+  useEffect(() => {
+    dispatch(getAllOrders());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getAllOrders())
-      }, [dispatch]);
+  const OrderByUser = Orders.filter((e) => e.email === user.email);
 
+  return (
+    <div>
+      {OrderByUser.map((row) => {
+        return (
+          <div key={row.id}>
+            <h3>Purchase History</h3>
 
-      const OrderByUser = Orders.filter(e => e.email === user.email );
+            <ul style={{ display: "flex", flexDirection: "row" }}>
+              <li>
+                <TableCell>
+                  <strong>Title:</strong>{" "}
+                  {row.items.map((r) => (
+                    <div>{r.title}</div>
+                  ))}
+                </TableCell>
+              </li>
+              <br />
+              <li>
+                <TableCell>
+                  <strong>Quantity:</strong>{" "}
+                  {row.items.map((r) => (
+                    <div>{r.quantity}</div>
+                  ))}
+                </TableCell>
+              </li>
+              <br />
+              <li>
+                <TableCell>
+                  <strong>Price:</strong>{" "}
+                  {row.items.map((r) => (
+                    <div>{r.price}</div>
+                  ))}
+                </TableCell>
+              </li>
+              <br />
+              <br />
 
-      return (
-        <div>
-
-        {
-            OrderByUser.map(row => {
-            return (
-                <div key={row.id}>
-                   
-                    <h3>Purchase History</h3>
-      
-
-  <ul style={{display: 'flex', flexDirection: 'row'}}>
-   <li>
-     <TableCell>
-       <strong>Title:</strong> {row.items.map((r) => <div>{r.title}</div>)}
-     </TableCell>
-   </li>
-   <br />
-   <li>
-     <TableCell>
-       <strong>Quantity:</strong> {row.items.map((r) => <div>{r.quantity}</div>)}
-     </TableCell>
-   </li>
-   <br />
-   <li>
-     <TableCell>
-       <strong>Price:</strong> {row.items.map((r) => <div>{r.price}</div>)}
-     </TableCell>
-   </li>
-   <br />
-   <br />
-  
-   <li>
-     <TableCell>
-       <strong>Total :</strong>  {row.items.map((r) => (r.price * r.quantity)).reduce((a, b) => a + b, 0)}
-     </TableCell>
-   </li>
-   <li>
-     <TableCell>
-       <strong>Status:   </strong> {row.status}
-     </TableCell>
-   </li>
-   <br />
-   <li>
-     <TableCell>
-       <strong>Date:     </strong> {row.createdAt}
-     </TableCell>
-   </li>
-   
-</ul>
-
-
-                  
-                
-                </div>
-            )
-        })}
-
-        </div>
-        
-
-      )
-
+              <li>
+                <TableCell>
+                  <strong>Total :</strong>{" "}
+                  {row.items
+                    .map((r) => r.price * r.quantity)
+                    .reduce((a, b) => a + b, 0)}
+                </TableCell>
+              </li>
+              <li>
+                <TableCell>
+                  <strong>Status: </strong> {row.status}
+                </TableCell>
+              </li>
+              <br />
+              <li>
+                <TableCell>
+                  <strong>Date: </strong> {row.createdAt}
+                </TableCell>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-
 
 export default OrderHistory;
